f909b366be2781d3c34e70f08e6d8a6a55b48a12
Chintan Patel <chintanpatelit024@gmail.com>
2025-11-11 17:46:35 +0530

    Added: Fog for render distance

--- Assets/Scripts/World Generation/World/World.cs
+++ Assets/Scripts/World Generation/World/World.cs
@@ -1,18 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using VoxelWorld.WorldGeneration.Chunks;
 
-public class World : MonoBehaviour
+namespace VoxelWorld.WorldGeneration
 {
-    // Start is called before the first frame update
-    void Start()
+    public class World : MonoBehaviour
     {
-        
-    }
+        [Header("World Settings")]
+        public int worldSeed = 12345;
+        public int renderDistance = 4;
+        public ChunkLoader chunkLoader;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [Header("Atmosphere Settings")]
+        public Color fogColor = new(0.7f, 0.8f, 0.9f);
+        public bool useLinearFog = true;
+        public float fogDensity = 0.02f;
+
+        private void Awake()
+        {
+            Random.InitState(worldSeed);
+            SetupFog();
+        }
+
+        public void SetupFog()
+        {
+            RenderSettings.fog = true;
+            RenderSettings.fogColor = fogColor;
+
+            if (useLinearFog)
+            {
+                RenderSettings.fogMode = FogMode.Linear;
+                RenderSettings.fogStartDistance = (renderDistance - 1) * Chunk.chunkSize;
+                RenderSettings.fogEndDistance = renderDistance * Chunk.chunkSize * 1.2f;
+            }
+            else
+            {
+                RenderSettings.fogMode = FogMode.Exponential;
+                RenderSettings.fogDensity = fogDensity;
+            }
+
+            Debug.Log($"Fog configured for worldSeed {worldSeed}");
+        }
+
+        public void ApplyBiomeFog(Color color, float startMultiplier, float endMultiplier)
+        {
+            RenderSettings.fogColor = color;
+            RenderSettings.fogStartDistance = renderDistance * Chunk.chunkSize * startMultiplier;
+            RenderSettings.fogEndDistance = renderDistance * Chunk.chunkSize * endMultiplier;
+        }
     }
 }
